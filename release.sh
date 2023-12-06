46e04eb7094318b75bda6e3453114fda3a4449f1
galacticclam <galacticclam>
2023-12-06 01:21:07 -0700

    Improve release script

--- release.sh
+++ release.sh
@@ -4,11 +4,22 @@ set -euo pipefail
 
 version=$(grep "Version: " TwitchEmotes_Coomer.toc | cut -d ' ' -f 3)
 
+[[ -z $(git status -s) ]] || {
+    echo "Error: Not all changes committed"
+    exit 1
+}
+
+git merge-base --is-ancestor HEAD @{u} || { 
+    echo "Error: Not all commits merged"
+    exit 1
+}
+
 ./build.sh
 
 gh release create \
     "$version" \
     --title "$version" \
     --notes "$version" \
-    --draft \
     "dist/TwitchEmotes_Coomer-$version.zip"
+
+echo "Published version $version"
