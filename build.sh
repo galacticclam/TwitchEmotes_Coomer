5b1f5b1e3b43f3c05518ea19b724f56423dfec5e
galacticclam <galacticclam>
2023-12-06 12:10:14 -0700

    Add Classic TOC file

--- build.sh
+++ build.sh
@@ -2,7 +2,15 @@
 
 set -euo pipefail
 
-version=$(grep "Version: " TwitchEmotes_Coomer.toc | cut -d ' ' -f 3)
+version_mainline=$(grep "Version: " TwitchEmotes_Coomer-Mainline.toc | cut -d ' ' -f 3)
+version_classic=$(grep "Version: " TwitchEmotes_Coomer-Classic.toc | cut -d ' ' -f 3)
+
+[ "$version_mainline" == "$version_classic" ] || {
+    echo "Error: TwitchEmotes_Coomer-Mainline.toc is version $version_mainline, but TwitchEmotes_Coomer-Classic.toc is version $version_classic"
+    exit 1
+}
+
+version=$version_mainline
 
 dist_root="dist"
 dist_dir="$dist_root/TwitchEmotes_Coomer"
@@ -13,6 +21,7 @@ mkdir -p "$dist_dir"
 cp -r emotes "$dist_dir"
 cp emotes.lua "$dist_dir"
 cp main.lua "$dist_dir"
-cp TwitchEmotes_Coomer.toc "$dist_dir"
+cp TwitchEmotes_Coomer-Mainline.toc "$dist_dir"
+cp TwitchEmotes_Coomer-Classic.toc "$dist_dir"
 
 powershell Compress-Archive "$dist_dir" "$zip_name"
--- release.sh
+++ release.sh
@@ -2,14 +2,22 @@
 
 set -euo pipefail
 
-version=$(grep "Version: " TwitchEmotes_Coomer.toc | cut -d ' ' -f 3)
+version_mainline=$(grep "Version: " TwitchEmotes_Coomer-Mainline.toc | cut -d ' ' -f 3)
+version_classic=$(grep "Version: " TwitchEmotes_Coomer-Classic.toc | cut -d ' ' -f 3)
+
+[ "$version_mainline" == "$version_classic" ] || {
+    echo "Error: TwitchEmotes_Coomer-Mainline.toc is version $version_mainline, but TwitchEmotes_Coomer-Classic.toc is version $version_classic"
+    exit 1
+}
+
+version="$version_mainline"
 
 [[ -z $(git status -s) ]] || {
     echo "Error: Not all changes committed"
     exit 1
 }
 
-git merge-base --is-ancestor HEAD @{u} || { 
+git merge-base --is-ancestor HEAD @{u} || {
     echo "Error: Not all commits merged"
     exit 1
 }
