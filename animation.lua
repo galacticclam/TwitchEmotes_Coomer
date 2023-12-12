ff70ae7f20b81ff34db30f6ac3911ea8997bde86
galacticclam <galacticclam>
2023-12-11 17:10:53 -0700

    Fix speed of SCHIZO and MONKE

--- animation.lua
+++ animation.lua
@@ -3,8 +3,8 @@ TwitchEmotes_animation_metadata[basePath .. "peepoRun.tga"] = {["nFrames"] = 6, 
 TwitchEmotes_animation_metadata[basePath .. "donkWalk.tga"] = {["nFrames"] = 10, ["frameWidth"] = 32, ["frameHeight"] = 32, ["imageWidth"] = 32, ["imageHeight"] = 512, ["framerate"] = 18}
 TwitchEmotes_animation_metadata[basePath .. "catPls.tga"] = {["nFrames"] = 30, ["frameWidth"] = 32, ["frameHeight"] = 32, ["imageWidth"] = 32, ["imageHeight"] = 1024, ["framerate"] = 18}
 TwitchEmotes_animation_metadata[basePath .. "Cooking.tga"] = {["nFrames"] = 10, ["frameWidth"] = 32, ["frameHeight"] = 32, ["imageWidth"] = 32, ["imageHeight"] = 512, ["framerate"] = 18}
-TwitchEmotes_animation_metadata[basePath .. "MONKE.tga"] = {["nFrames"] = 158, ["frameWidth"] = 32, ["frameHeight"] = 32, ["imageWidth"] = 32, ["imageHeight"] = 8192, ["framerate"] = 60}
-TwitchEmotes_animation_metadata[basePath .. "SCHIZO.tga"] = {["nFrames"] = 71, ["frameWidth"] = 32, ["frameHeight"] = 32, ["imageWidth"] = 32, ["imageHeight"] = 4096, ["framerate"] = 60}
+TwitchEmotes_animation_metadata[basePath .. "MONKE.tga"] = {["nFrames"] = 158, ["frameWidth"] = 32, ["frameHeight"] = 32, ["imageWidth"] = 32, ["imageHeight"] = 8192, ["framerate"] = 45}
+TwitchEmotes_animation_metadata[basePath .. "SCHIZO.tga"] = {["nFrames"] = 71, ["frameWidth"] = 32, ["frameHeight"] = 32, ["imageWidth"] = 32, ["imageHeight"] = 4096, ["framerate"] = 45}
 TwitchEmotes_animation_metadata[basePath .. "catYep.tga"] = {["nFrames"] = 2, ["frameWidth"] = 32, ["frameHeight"] = 32, ["imageWidth"] = 32, ["imageHeight"] = 64, ["framerate"] = 14}
 TwitchEmotes_animation_metadata[basePath .. "catNope.tga"] = {["nFrames"] = 2, ["frameWidth"] = 32, ["frameHeight"] = 32, ["imageWidth"] = 32, ["imageHeight"] = 64, ["framerate"] = 14}
 TwitchEmotes_animation_metadata[basePath .. "FeelsWeakMan.tga"] = {["nFrames"] = 49, ["frameWidth"] = 32, ["frameHeight"] = 32, ["imageWidth"] = 32, ["imageHeight"] = 2048, ["framerate"] = 18}
