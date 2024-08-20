ff4e6fe0c836b6312e4fa0a29ee3a4a67fee8e8a
galacticclam <galacticclam>
2024-08-19 21:17:33 -0600

    TWW prepatch

--- import.sh
+++ import.sh
@@ -4,12 +4,14 @@ set -euo pipefail
 
 image="emotes/$2.webp"
 
-size=${3:-32}
+size=${3:-64}
 
 url=$(echo "$1" | sed "s/size=[[:digit:]]\+/size=$size/")
 curl "$url" -o "emotes/$2.webp"
 
-magick "$image" -background none -gravity center -resize "${size}x${size}" "$image"
+# magick "$image" -background none -gravity center -resize "${size}x${size}" "$image"
+
+magick "$image" -background none -gravity center -extent "128x64" "$image"
 
 newline='["'$2'"] = basePath .. "'$2'.tga:28:28",'
 
